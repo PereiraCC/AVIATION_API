183d85ba6269c774b8364fb1ce4f28ede1044215
Carlos Pereira <carloscoto0103@gmail.com>
2022-02-09 17:33:28 -0600

    First commit: controller AIRPORT with two actions get all and get an airport

---
+++ AirportsAPI/Controllers/AIRPORTSController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AirportsAPI.Models;
+
+namespace AirportsAPI.Controllers
+{
+    public class AIRPORTSController : ApiController
+    {
+        private AVIATION_COSTARICAEntities1 db = new AVIATION_COSTARICAEntities1();
+
+        // GET: api/AIRPORTS
+        public IHttpActionResult GetAIRPORTS()
+        {
+            return Json(db.AIRPORTS.ToList());
+        }
+
+        // GET: api/AIRPORTS/5
+        [ResponseType(typeof(AIRPORTS))]
+        public IHttpActionResult GetAIRPORTS(int id)
+        {
+            AIRPORTS aIRPORTS = db.AIRPORTS.Find(id);
+            if (aIRPORTS == null)
+            {
+                return NotFound();
+            }
+
+            return Json(aIRPORTS);
+        }
+
+    }
+}
